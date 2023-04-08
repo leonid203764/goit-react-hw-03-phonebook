499d06b680ebff8aad25d79129757a5eb26cbd99
Leonid <108736813+leonid203764@users.noreply.github.com>
2023-04-08 17:31:49 +0300

    up

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,22 +1,17 @@
 import PropTypes from 'prop-types';
-import css from '../Filter/Filter.module.css';
 
-const Filter = ({ value, onChange }) => {
+import React from 'react';
+
+import { Input, Label } from './Filter.style';
+
+export const Filter = ({ value, onChange }) => {
   return (
-    <label className={css.label}>
-      Find contacts by name
-      <input
-        className={css.input}
-        type="text"
-        placeholder="Enter name..."
-        value={value}
-        onChange={onChange}
-      />
-    </label>
+    <Label>
+      Find contacts by name:
+      <Input type="text" value={value} onChange={onChange} />
+    </Label>
   );
 };
-
-export default Filter;
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
